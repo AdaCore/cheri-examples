c1df266b6afb5a3d21b1eb925eafde11c5a89750
Johannes Kliemann <kliemann@adacore.com>
2025-02-11 17:44:27 +0100

    Add single-application heap corruption example for BSD
    ref eng/projects/sabd/cheri-examples#3

---
+++ cheribsd_heap_corruption/src/alloc.h
@@ -0,0 +1,12 @@
+/**
+ *  Copyright (C) 2020-2025, AdaCore
+ *
+ *  SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+ **/
+
+#ifndef __ALLOC_H__
+#define __ALLOC_H__
+
+void *alloc(size_t);
+
+#endif
